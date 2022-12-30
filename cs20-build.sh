1d89418a2e6a81289be2db7a8358298564063456
J. David Eisenberg <jdavid.eisenberg@gmail.com>
2022-12-29 21:19:38 -0800

    Rewrite for Chapter 1 (General Intro).

---
+++ cs20-build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+~/pretext/pretext/pretext --format html --component all --directory output/html -v --publication pretext/publication-rs-for-all.xml pretext/cs20-thinkcspy.ptx > build_errors.txt 2>&1
+
