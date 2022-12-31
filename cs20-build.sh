7b6929d568bbc697989a55074f43284ef64d77a6
J. David Eisenberg <jdavid.eisenberg@gmail.com>
2022-12-31 05:32:32 -0800

    Various changes.

--- cs20-build.sh
+++ cs20-build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-~/pretext/pretext/pretext --format html --component all --directory output/html -v --publication pretext/publication-rs-for-all.xml pretext/cs20-thinkcspy.ptx > build_errors.txt 2>&1
+~/pretext/pretext/pretext --format html --component all \
+	 -v --publication pretext/publication-rs-for-all.xml pretext/cs20-thinkcspy.ptx > build_errors.txt 2>&1
 
