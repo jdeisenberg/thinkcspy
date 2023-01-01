d8df0467736ea2f234486e0845f7c8260b48d9f8
J. David Eisenberg <jdavid.eisenberg@gmail.com>
2022-12-31 21:48:58 -0800

    Continued changes to chapter 2.

--- cs20-build.sh
+++ cs20-build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ~/pretext/pretext/pretext --format html --component all \
+	--directory output/html \
 	 -v --publication pretext/publication-rs-for-all.xml pretext/cs20-thinkcspy.ptx > build_errors.txt 2>&1
 
